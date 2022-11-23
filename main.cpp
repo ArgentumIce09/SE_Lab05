7061609ebc15d8e3858e197c2f3363bf53af3573
Shekin Andrew <shekin_andrei@mail.ru>
2022-11-23 20:46:32 +0500

    Multiplication A & B

--- main.cpp
+++ main.cpp
@@ -12,5 +12,6 @@ int main()
 
     cout << "A + B = " << a + b << endl;
     cout << "A - B = " << a - b << endl;
+    cout << "A * B = " << a * b << endl; // multiplication
     cout << "A / B = " << a / b << endl; // Division
 }
