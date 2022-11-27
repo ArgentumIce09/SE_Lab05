a7c27779e3d2be605fe4cfdeda4cc584685a86e2
Shekin Andrew <shekin_andrei@mail.ru>
2022-11-27 21:32:33 +0500

    Coordinates body

--- main.cpp
+++ main.cpp
@@ -1,17 +1,31 @@
 
 /* +++++ Education program +++++ */
 #include <iostream>
-
+#include <cmath>
 using namespace std;
+
+const double g = 9.8;
+const double pi = 3.141;
+double v, alpha, T; // velocity, angle, time
+double t, x, y; // Moment time, coordinates body
+
 
 int main()
 {
-    int a, b;
-    cout << "Enter A and B: " << endl;
-    cin >> a >> b;
 
-    cout << "A + B = " << a + b << endl; // Addition
-    cout << "A - B = " << a - b << endl; // Subtraction
-    cout << "A * B = " << a * b << endl; // Multiplication
-    cout << "A / B = " << a / b << endl; // Division
+    cout << "Enter speed v = " << endl;
+    cin >> v;
+    cout << "Enter angle alpha = " << endl;
+    cin >> alpha;
+    alpha = alpha*pi/180;
+    T=2*v*sin(alpha)/g;
+    cout << "Enter time t = " <<  endl;
+    cin >> t;
+    cout << "*** Coordinates body *** " << endl;
+    x=v*t*cos(alpha);
+    y=v*t*sin(alpha)-g*t*t/2;
+    cout << "x = " << x << endl;
+    cout << "y = " << y << endl;
+
+    return 0;
 }
